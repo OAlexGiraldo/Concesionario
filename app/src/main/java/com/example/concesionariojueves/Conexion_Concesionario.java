eda1485c3c17962be7a4a017a50c1007541957ea
206 <206@academicos.cesde.edu.co>
2022-02-24 08:13:58 -0500

    concecionario

---
+++ app/src/main/java/com/example/concesionariojueves/Conexion_Concesionario.java
@@ -0,0 +1,29 @@
+package com.example.concesionariojueves;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Conexion_Concesionario  extends SQLiteOpenHelper {
+
+    public Conexion_Concesionario(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("create table TbLCliente(identificacion text primary key,nombre text not null,"+
+                "usuario text not null,clave text not null,activo text not null default'si')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE Tblclientes");{
+          onCreate(sqLiteDatabase);
+        }
+
+    }
+}
